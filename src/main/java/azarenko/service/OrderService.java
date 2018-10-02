885fb74a100a058d7cc6621381054c1ff155173d
Anton Azarenka <Anton_Azarenka@epam.com>
2018-10-02 09:24:18 +0300

    Add ServiceImpl order

--- src/main/java/azarenko/service/OrderService.java
+++ src/main/java/azarenko/service/OrderService.java
@@ -1,4 +1,16 @@
 package azarenko.service;
 
+import azarenko.entity.Order;
+
+import java.util.List;
+
 public interface OrderService {
+    Order getByID (String id);
+
+    List<Order> getAll();
+
+    List<Order> getByAuthor(String name);
+
+    Order getByName(String name);
+
 }
---
+++ src/main/java/azarenko/service/logic/OrderServiceImpl.java
@@ -0,0 +1,37 @@
+package azarenko.service.logic;
+
+import azarenko.entity.Order;
+import azarenko.repository.OrderRepository;
+import azarenko.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    @Override
+    public Order getByID(String id) {
+        return repository.read(id);
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return repository.readAll();
+    }
+
+    @Override
+    public List<Order> getByAuthor(String name) {
+        return repository.getByAuthor(name);
+    }
+
+    @Override
+    public Order getByName(String name) {
+        return repository.getByName(name);
+    }
+}
