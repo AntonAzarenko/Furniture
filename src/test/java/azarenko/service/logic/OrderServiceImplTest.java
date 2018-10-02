9b9e1fa29791e69b0c5fd6b72f0ee431d8a650cb
Anton Azarenka <Anton_Azarenka@epam.com>
2018-10-02 09:29:25 +0300

    Add Service test

---
+++ src/test/java/azarenko/service/logic/OrderServiceImplTest.java
@@ -0,0 +1,24 @@
+package azarenko.service.logic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderServiceImplTest {
+
+    @Test
+    public void getByID() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void getByAuthor() {
+    }
+
+    @Test
+    public void getByName() {
+    }
+}
