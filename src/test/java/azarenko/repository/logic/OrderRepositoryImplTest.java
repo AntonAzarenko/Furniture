2b503e81bf10db9efb50b667f9b76ff64b361b31
Anton Azarenka <Anton_Azarenka@epam.com>
2018-10-02 09:15:27 +0300

    Add test delete order

--- src/test/java/azarenko/repository/logic/OrderRepositoryImplTest.java
+++ src/test/java/azarenko/repository/logic/OrderRepositoryImplTest.java
@@ -4,6 +4,8 @@ import azarenko.entity.Order;
 import azarenko.repository.OrderRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,13 +17,18 @@ import static testdata.DataOrder.orderTest;
 @SpringBootApplication
 public class OrderRepositoryImplTest {
 
+    private final static Logger log = LoggerFactory.getLogger(DetailRepositoryImplTest.class);
+
     @Autowired
     private OrderRepository repository;
 
     @Test
     public void getByName() {
         Order order = repository.getByName("Камод");
+        log.info(order.toString());
         DataOrder.assertMatch(order, orderTest);
+
+
     }
 
     @Test
@@ -51,5 +58,7 @@ public class OrderRepositoryImplTest {
 
     @Test
     public void delete() {
+        Order order = repository.getByName("Камод");
+        repository.delete(order.getId());
     }
 }
